22269afb4800533858cd8df7205ce43434954718
Jim Jo <jimjolo2tt@gmail.com>
2026-01-13 21:45:41 +0100

    Add actual item registration to Forge mod
    - Added RegistryObject<Item> declarations for 7 drug items
    - Items: weed, cocaine, heroin, meth, lsd, ecstasy, xanax
    - Fixed type compatibility issues
    - This should resolve 'no items in creative mode' issue

--- forge-mdk/src/main/java/com/example/examplemod/ExampleMod.java
+++ forge-mdk/src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,6 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +16,23 @@ public class ExampleMod {
     // Create a Deferred Register to hold Items
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "drugsmod");
 
+    // Register some basic drug items
+    public static final RegistryObject<Item> WEED = ITEMS.register("weed", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> COCAINE = ITEMS.register("cocaine", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> HEROIN = ITEMS.register("heroin", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> METH = ITEMS.register("meth", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> LSD = ITEMS.register("lsd", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ECSTASY = ITEMS.register("ecstasy", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> XANAX = ITEMS.register("xanax", () -> new Item(new Item.Properties()));
+
     public ExampleMod() {
         // Get the mod event bus
         var modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         
         // Register our items
         ITEMS.register(modEventBus);
+        
         LOGGER.info("Drugs Mod loaded successfully!");
+        LOGGER.info("Registered {} drug items", 7);
     }
 }
